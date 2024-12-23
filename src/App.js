59530e984fa63dba963c89bc89be4517e45e5ee1
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-23 04:23:59 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from "./components/Navbar/Navbar.jsx"
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+
+    </div>
+  );
+}
+
+export default App;
