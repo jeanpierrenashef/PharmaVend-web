be09118902d73e225bc05ed93443ad0de819a0cd
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-23 07:21:26 +0300

    Added the content in the mission jsx file

--- src/components/Mission/Mission.jsx
+++ src/components/Mission/Mission.jsx
@@ -4,7 +4,27 @@ import "../base.css"
 
 const Mission = () => {
     return(
+        <section class="mission">
+            <h1>Our Mission</h1>
+            <div class="mission-container">
+                <div class="mission-text left">
+                    <p>Provide 24/7 access to essential medications and medical supplies,</p>
+                    <p>Leverage cutting-edge technology like robotics, IoT, and AI to deliver</p>
+                </div>
+
+            
+                <div class="mission-image">
+                    <img src="../Assets/capsule.png" />
+                </div>
+
+                
+                <div class="mission-text right">
+                    <p>Bridge the gap in healthcare accessibility, addressing</p>
+                    <p>Empower communities by making healthcare more convenient, secure needs.</p>
+                </div>
+            </div>
+        </section>
 
     );
-}
+};
 export default Mission;
