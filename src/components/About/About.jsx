bfcac244fa2077c8aca6a25c2aac92a23c6c168b
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-23 06:00:46 +0300

    Initializing the header class

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,6 +5,18 @@ const About = () => {
 return (
     <section id="about" className="about">
         <h2>About Us</h2>
+        <div>
+            <div>
+                <h2>Revolutionizing Access to Medications</h2>
+                <h4>At PharmaVend, we are committed to redefining 
+                    how people access essential medications and medical
+                    supplies. Born from the need to provide reliable, 
+                    round-the-clock solutions, PharmaVend offers 
+                    state-of-the-art vending machines equipped with a 
+                    user-friendly interface and smart technology to dispense
+                    pharmaceutical essentials when and where you need them most.</h4>
+            </div>
+        </div>
         <p>We provide the best services in the industry.</p>
     </section>
 );
