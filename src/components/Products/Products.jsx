841574756e556b3309689ad8139a235e0931882c
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2024-12-23 09:08:59 +0300

    Done with the products section

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,31 +1,50 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Products.css";
+import "../base.css";
 
 const Products = () => {
-const products = [
-    { id: 1, image: "../../Assets/ph1.png" },
-    { id: 2, image: "../../Assets/ph2.png" },
-    { id: 3, image: "../../Assets/ph3.png" },
-    { id: 4, image: "../../Assets/ph4.png" },
-    { id: 5, image: "../../Assets/ph5.png" },
-    { id: 6, image: "../../Assets/ph6.png" },
-    { id: 7, image: "../../Assets/ph7.png" },
-    { id: 8, image: "../../Assets/ph8.png" },
-    { id: 9, image: "../../Assets/ph9.png" },
-];
+    const productsContainerRef = useRef(null);
 
-return (
-    <section className="products">
-        <h1>Our Products</h1>
-        <div className="products-container">
-            {products.map((product) => (
-            <div className="product-box" key={product.id}>
-                <img src={product.image} alt={`Brand ${product.id}`} />
+    const scrollLeft = () => {
+        productsContainerRef.current.scrollBy({
+        left: -200, 
+        behavior: "smooth",
+        });
+    };
+
+    const scrollRight = () => {
+        productsContainerRef.current.scrollBy({
+        left: 200, 
+        behavior: "smooth",
+        });
+    };
+
+    const products = Array.from({ length: 9 }, (_, index) => ({
+        id: index + 1,
+        image: require(`../../Assets/ph${index + 1}.png`),
+    }));
+
+    return (
+        <section className="flex column products">
+            <h1>Our Products</h1>
+            <h4>Our vending machines are stocked with a wide range of products from globally renowned pharmaceutical brands. From essential medications to healthcare supplies, these machines provide quick and easy access to trusted names such as Pfizer, GSK, AstraZeneca, and more. Designed to meet diverse healthcare needs, our machines ensure reliability and convenience around the clock, making high-quality products accessible to everyone.</h4>
+            <div className="scroll-buttons">
+                <button className="scroll-btn left" onClick={scrollLeft}>
+                &#8592;
+                </button>
+                <div className="products-container" ref={productsContainerRef}>
+                {products.map((product) => (
+                    <div className="product-box" key={product.id}>
+                    <img src={product.image} alt={`Brand ${product.id}`} />
+                    </div>
+                ))}
+                </div>
+                <button className="scroll-btn right" onClick={scrollRight}>
+                &#8594;
+                </button>
             </div>
-            ))}
-        </div>
-    </section>
-);
+        </section>
+    );
 };
 
 export default Products;
