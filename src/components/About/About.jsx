e86f2026ffdaed1e22a33cbeb6062b347804d2af
Jeanpierrenashef <jeanpierrenashef@gmail.com>
2025-01-14 00:32:23 +0300

    Added images to the website

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,35 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import MapComponent from './MapComponent';
 import './About.css';
 import '../base.css';
 
 const About = () => {
-return (
-    <section id="about" className="flex column about">
-        <h1>About Us</h1>
-        <div className="flex about-disp">
-            <div className="flex column about-text">
-                <h2>Revolutionizing Access to Medications</h2>
-                <h4>
-                    At PharmaVend, we are committed to redefining how people access
-                    essential medications and medical supplies. Born from the need to
-                    provide reliable, round-the-clock solutions, PharmaVend offers
-                    state-of-the-art vending machines equipped with a user-friendly
-                    interface and smart technology to dispense pharmaceutical essentials
-                    when and where you need them most.
-                </h4>
+    const [machines, setMachines] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://192.168.1.7:8000/api/display_machines")
+            .then(response => {
+                setMachines(response.data); 
+            })
+            .catch(error => {
+                console.error('Error fetching machines:', error);
+            });
+    }, []); 
+
+    return (
+        <section className="about-section">
+            <div id="about" className="flex column about">
+                <h1>About Us</h1>
+                <div className="flex about-disp">
+                    <div className="flex column about-text">
+                        <h2>Revolutionizing Access to Medications</h2>
+                        <h4>
+                            At PharmaVend, we are committed to redefining how people access
+                            essential medications and medical supplies. Born from the need to
+                            provide reliable, round-the-clock solutions, PharmaVend offers
+                            state-of-the-art vending machines equipped with a user-friendly
+                            interface and smart technology to dispense pharmaceutical essentials
+                            when and where you need them most.
+                        </h4>
+                    </div>
+                    <div className="map-container" style={{ width: '800px', height: '300px' }}>
+                        <MapComponent machines={machines} />
+                    </div>
+                </div>
             </div>
-            <div className="map-container">
-                <iframe
-                    title="PharmaVend Locations"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3164.9964828753476!2d-122.08385168469225!3d37.38605197983179!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fb0cb9c3d4d3b%3A0xfb2e0df6f6ec4e03!2sGoogleplex!5e0!3m2!1sen!2sus!4v1675341022549!5m2!1sen!2sus"
-                    style={{ border: 0, width: "100%", height: "100%" }}
-                    allowFullScreen=""
-                    loading="lazy"
-                ></iframe>
-            </div>
-        </div>
-    </section>
-);
+        </section>
+        
+    );
 };
 
 export default About;
---
+++ src/components/About/MapComponent.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+
+const MapComponent = ({ machines }) => {
+    const { isLoaded } = useJsApiLoader({
+        googleMapsApiKey: "AIzaSyBblu-ime4U7pTC4ILi9CJmQeAYkvBdmG0", 
+        
+    });
+    const center = { lat: 33.8892, lng: 35.4827 };
+    const zoomLevel = 8;
+
+    if (!isLoaded) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div style={{ width: "800px", height: "300px" }}>
+        <GoogleMap
+            mapContainerStyle={{ width: "100%", height: "100%" }}
+            center={center}
+            zoom={zoomLevel}
+        >
+            {machines.map((machine) => (
+            <Marker
+                key={machine.id}
+                position={{ lat: parseFloat(machine.latitude), lng: parseFloat(machine.longitude) }}
+                title={machine.location}
+            />
+            ))}
+        </GoogleMap>
+        </div>
+    );
+};
+
+export default MapComponent;
